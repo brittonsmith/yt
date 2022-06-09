ca7f7cb62c04d1eed5fac1ff9dd5abc3d42e6368
Britton Smith <brittonsmith@gmail.com>
2022-06-09 14:51:02 +0100

    Check is group exists before trying to get parameters from it.

--- yt/frontends/gadget_fof/data_structures.py
+++ yt/frontends/gadget_fof/data_structures.py
@@ -200,9 +200,10 @@ class GadgetFOFDataset(ParticleDataset):
             self.parameters = {
                 str(field): val for field, val in f["Header"].attrs.items()
             }
-            self.parameters.update(
-                {str(field): val for field, val in f["Parameters"].attrs.items()}
-            )
+            if "Parameters" in f:
+                self.parameters.update(
+                    {str(field): val for field, val in f["Parameters"].attrs.items()}
+                )
 
         self.dimensionality = 3
         self.refine_by = 2
